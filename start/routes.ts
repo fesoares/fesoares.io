7689b9f27a1b75a1ba86a623ed43a4223a1bec4c
Felipe Soares <felipe4dev@gmail.com>
2020-07-16 10:03:03 -0300

    starter commit

---
+++ start/routes.ts
@@ -0,0 +1,39 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes/index.ts` as follows
+|
+| import './cart'
+| import './customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
+
+Route.post('/contact', async () => {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'felipe4dev@gmail.com',
+    from: 'felipe4gameplay@gmail.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail.send(msg)
+  return { hello: 'world' }
+})
